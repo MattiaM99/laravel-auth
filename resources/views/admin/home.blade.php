f26e2f0111edeb754f1ed464d94fc597c74ae742
MattiaM99 <mattiamazzoni123@gmail.com>
2022-02-02 15:02:09 +0100

    innestate le rotte admin e guest

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Area admin
                 </div>
             </div>
         </div>
