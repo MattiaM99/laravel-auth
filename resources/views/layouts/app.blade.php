46e5768b9916130eea9a1c57c96df3c951c0d727
MattiaM99 <mattiamazzoni123@gmail.com>
2022-02-03 17:16:43 +0100

    posts visibili solo all'utente autenticato

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,10 @@
                 <a class="navbar-brand" href="{{ route('home') }}">
                   Vai al sito
                 </a>
+                @auth
+                <a href="{{route('admin.posts.index')}}">Elenco posts</a>
+                    
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
